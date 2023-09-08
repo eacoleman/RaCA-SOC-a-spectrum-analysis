65b3a190bd8f3f5e8927597dc84b3003d7ce2b26
Evan Coleman <evanacoleman@gmail.com>
2023-09-07 23:31:49 -0400

    Add step 9 analysis including initial VAE run without uncertainty estimation.

--- RaCA-SOC-a/get-RaCA-VNIR-data.R
+++ RaCA-SOC-a/get-RaCA-VNIR-data.R
@@ -7,12 +7,12 @@ library(reshape2)
 library(cluster)
 library(dplyr)
 
-seriesNameCSV <- read.csv("../data/RaCA-series-names.csv",sep=',')
-# seriesNameCSV <- list('auburn')#,'bbb','christian')
+# seriesNameCSV <- read.csv("RaCA-series-names.csv",sep=',')
+seriesNameCSV <- list('auburn')#,'bbb','christian')
 dl <- list()
 
-for(tser in rownames(seriesNameCSV)) {
-# for(tser in seriesNameCSV) {
+# for(tser in colnames(seriesNameCSV)) {
+for(tser in seriesNameCSV) {
 
   print("---------------------------\n")
   print(tser)
@@ -25,19 +25,19 @@ for(tser in rownames(seriesNameCSV)) {
 
         # d contains spectral data
         # merge pedon location data (x,y) by rcapid entry
-        pedons_data <- data.frame(rcapid = character(0), x = numeric(0), y = numeric(0))
+        pedons_data <- data.frame(rcapid = character(0), x = numeric(0), y = numeric(0), pedon_id = character(0))
         
         for (i in 1:length(r.alldat$pedons)) {
           current_profile <- r.alldat$pedons[i]
           
           if (!is.null(current_profile$x) && !is.null(current_profile$y)) {
-            pedons_data <- rbind(pedons_data, data.frame(rcapid = current_profile$rcapid, x = current_profile$x, y = current_profile$y))
+            pedons_data <- rbind(pedons_data, data.frame(rcapid = current_profile$rcapid, x = current_profile$x, y = current_profile$y, pedon_id=current_profile$pedon_id))
           }
         }
 
         # Remove duplicates
         pedons_data <- pedons_data %>%
-          distinct(rcapid, .keep_all = TRUE)
+         distinct(pedon_id, .keep_all = TRUE)
 
         # Merge sample data with pedon data
         sample_data <- r.alldat$sample %>% left_join(pedons_data, by='rcapid')
@@ -56,13 +56,13 @@ for(tser in rownames(seriesNameCSV)) {
   },
   finally={
     if(exists("r.alldat")) {
-      remove(r.alldat)
-      remove(pedons_data)
-      remove(sample_data)
-      remove(d)
+      # remove(r.alldat)
+      # remove(pedons_data)
+      # remove(sample_data)
+      # remove(d)
       next
     }
   })
 }
 
-write.table(dl, file="./RaCA-dataset-with-locations.txt", row.names=F, sep=",")
+# write.table(dl, file="./RaCA-dataset-with-locations.txt", row.names=F, sep=",")
